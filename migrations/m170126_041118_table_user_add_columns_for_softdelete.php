ce27939a914882cd4350cddc0401b6114eafda0a
Nex Otaku <nex@otaku.ru>
2017-01-27 09:06:04 +0300

    Исправил опечатку.

--- migrations/m170126_041118_table_user_add_columns_for_softdelete.php
+++ migrations/m170126_041118_table_user_add_columns_for_softdelete.php
@@ -2,7 +2,7 @@
 
 use dmstr\db\mysql\FileMigration;
 
-class m170126_041118_table_feed_add_columns_for_softdelete extends FileMigration
+class m170126_041118_table_user_add_columns_for_softdelete extends FileMigration
 {
     # create a sql file `m170126_041118_table_feed_add_columns_for_softdelete2.sql` or adjust and uncomment the following line, do not change this class name
     //public $file = 'custom-filename.sql';
